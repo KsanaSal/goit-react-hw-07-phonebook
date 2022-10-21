1a4f6c0b0d96f2e85c7b690c41c116a84499417b
KsanaSal <oasalivon@gmail.com>
2022-10-21 20:52:45 +0300

    Fix bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,12 +45,13 @@ export class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
-    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (this.state.contacts !== prevState.contacts) {
-      console.log('test');
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
